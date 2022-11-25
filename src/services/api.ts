614ea59ef51689fb1b2f3821df6ccabd1241a800
Lipzs <filipedias823@hotmail.com>
2022-11-25 15:58:39 -0300

    changed url api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://ec2-44-206-254-244.compute-1.amazonaws.com:3333'
+  baseURL: 'http://ec2-54-158-162-69.compute-1.amazonaws.com:3333'
 });
 
 export default api;
